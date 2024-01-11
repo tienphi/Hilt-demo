08a3d9e896595a1bd548a10025bbbe1c9cf1cc55
tienphi <dactienbkba04@gmail.com>
2024-01-11 07:48:46 +0700

    Add some composable

--- app/src/main/java/com/example/hiltbasicdemo/ui/MainActivity.kt
+++ app/src/main/java/com/example/hiltbasicdemo/ui/MainActivity.kt
@@ -3,13 +3,24 @@ package com.example.hiltbasicdemo.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.hiltbasicdemo.ui.theme.HiltBasicDemoTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -44,5 +55,52 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     HiltBasicDemoTheme {
         Greeting("Android")
+    }
+}
+
+@Composable
+fun WidgetType(name: String, modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+            .padding(8.dp)
+            .background(
+                color = MaterialTheme.colorScheme.primary)
+    ) {
+        Text(text = name)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WidgetTypePreview() {
+    HiltBasicDemoTheme {
+        WidgetType(name = "Weather")
+    }
+}
+
+@Composable
+fun ListWidgets(widgets: List<String>, modifier: Modifier = Modifier) {
+    if (widgets.isEmpty()) {
+        Text(text = "Not have any widgets!")
+    } else {
+        LazyVerticalGrid(
+            columns = GridCells.Adaptive(100.dp),
+            horizontalArrangement = Arrangement.spacedBy(24.dp),
+            verticalArrangement = Arrangement.spacedBy(24.dp)
+        ) {
+            items(widgets){widget ->
+                WidgetType(name = widget)
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 350, heightDp = 700)
+@Composable
+fun ListWidgetsPreview() {
+    HiltBasicDemoTheme {
+        ListWidgets(widgets = listOf("Weather", "Music", "Compass"))
     }
 }
